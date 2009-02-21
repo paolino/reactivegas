f9d3bd3b6c5bb7cca58ed4c009d7716babd34c3d
paolino <paolino@paolinoub.(none)>
2009-02-21 15:08:16 +0100

    boh

--- Eventi.hs
+++ Eventi.hs
@@ -2,34 +2,34 @@
 module Eventi where
 
 import Control.Monad.State 
-import Codec.Crypto.RSA
+-- import Control.Monad.Error
+import Control.Monad.Writer
 import Control.Arrow
 import Data.Maybe
 import Data.List
 import Control.Applicative
 import Data.Char
+
 import qualified Text.ParserCombinators.ReadP as P
-import Text.CSV
-import Data.Set as S
-import Data.Map as M
-import Data.ByteString.Lazy.Char8
-import qualified Text.CSV
+import qualified Data.Set as S
+import qualified Data.Map as M
 
+import Codec.Crypto.RSA (PublicKey)
+import Text.CSV (parseCSV, printCSV)
+import Lib (injectM)
 
-import Crypto 
 import Fields
-
+--------------------------------------------------------------------------------
 data Evento
-	= Accredito 	{tempo :: Tempo, resp :: User, membro :: User , valore :: Valore, firma :: Firma} -- ^ il firmante ha ricevuto denaro
-	| Richiesta 	{tempo :: Tempo, resp :: User, membro :: User, bene :: Bene, valore :: Valore, firma :: Firma}	-- ^ 
-	| Apertura 	{tempo :: Tempo, resp :: User, bene :: Bene, firma :: Firma}
-	| Membro 	{tempo :: Tempo, resp :: User, membro :: User, firma :: Firma}
-	| Saldo 	{tempo :: Tempo, resp :: User, membro :: User, valore :: Valore, firma :: Firma}
-	| Responsabile 	{tempo :: Tempo, resp :: User, membro :: User, publicKey :: PublicKey, firma :: Firma}
-	| Chiusura 	{tempo :: Tempo, resp :: User, bene :: Bene, firma :: Firma}
-	| Fallimento 	{tempo :: Tempo, resp :: User, bene :: Bene, firma :: Firma}
+	= Accredito 	{tempo :: Tempo, responsabile :: User, membro :: User , valore :: Valore, firma :: Firma} 
+	| Richiesta 	{tempo :: Tempo, responsabile :: User, membro :: User, bene :: Bene, valore :: Valore, firma :: Firma}	
+	| Apertura 	{tempo :: Tempo, responsabile :: User, bene :: Bene, firma :: Firma}
+	| Membro 	{tempo :: Tempo, responsabile :: User, membro :: User, firma :: Firma}
+	| Saldo 	{tempo :: Tempo, responsabile :: User, membro :: User, valore :: Valore, firma :: Firma}
+	| Responsabile 	{tempo :: Tempo, responsabile :: User, membro :: User, publicKey :: PublicKey, firma :: Firma}
+	| Chiusura 	{tempo :: Tempo, responsabile :: User, bene :: Bene, firma :: Firma}
+	| Fallimento 	{tempo :: Tempo, responsabile :: User, bene :: Bene, firma :: Firma}
 	| Bootstrap 	{membro :: User, publicKey :: PublicKey}
-
 
 instance Read Evento where
 	readsPrec _ s = case parseCSV "read instance" s of
@@ -37,50 +37,48 @@ instance Read Evento where
 		Right (("Richiesta":t:r:m:b:v:f:[]):_) -> [(Richiesta (read t) (read r) (read m) (read b) (read v) (read f),"")]
 		Left t -> error $ show t
 instance Show Evento where		
-	show (Membro t r u f) = (printCSV. return) ["Membro", show t, show r, show u, show f]
-	show (Responsabile t r u pk f) = (printCSV. return) ["Responsabile", show t, show r, show u, show pk, show f]
-	show (Apertura t r o f) = (printCSV. return) ["Apertura", show t, show r, show o, show f]
-	show (Chiusura t r o f) = (printCSV. return) ["Chiusura", show t, show r, show o, show f]
-	show (Accredito t r u v f) = (printCSV. return) ["Accredito", show t, show r, show u , show v, show f]
-	show (Richiesta t r u v o f) = (printCSV. return) ["Richiesta", show t, show r ,show u, show v, show o, show f]
-	show (Saldo t r u v f) = (printCSV. return) ["Saldo", show t, show r, show u , show v, show f]
-	show (Bootstrap u pk) = (printCSV. return) ["Bootstrap", show u, show pk]
-	
+	show (Bootstrap u pk) = (init . printCSV. return) ["Bootstrap", show u, show pk]
+	show x =  (init . printCSV. return) (showU x ++ [show . firma $ x])
 
+showU (Membro t r u f) 		=  ["Membro", show t, show r, show u]
+showU (Responsabile t r u pk f) =  ["Responsabile", show t, show r, show u, show pk]
+showU (Apertura t r o f) 	=  ["Apertura", show t, show r, show o]
+showU (Chiusura t r o f) 	=  ["Chiusura", show t, show r, show o]
+showU (Accredito t r u v f) 	=  ["Accredito", show t, show r, show u , show v]
+showU (Richiesta t r u v o f) 	=  ["Richiesta", show t, show r ,show u, show v, show o]
+showU (Saldo t r u v f) 	=  ["Saldo", show t, show r, show u , show v]
 	
-
+-----------------------------------------------------------------------------------------
+	
 type Conoscenza = [Evento]
-
-type Chiavi = Map User PublicKey
 
 data Report  = Fallito {beneR :: Bene} | Successo {beneR :: Bene} deriving (Eq,Ord,Show,Read)
 
-data Beni = Beni {aperti :: Map Bene [Evento] , chiusi 	:: Set Report} deriving (Read,Show)
+tuttibeni :: Estratto -> S.Set Bene
+tuttibeni (Estratto as cs _ _ _ _) = S.union (S.fromList $ M.keys as) (S.map beneR cs)
 
-tuttibeni :: Beni -> Set Bene
-tuttibeni (Beni as cs) = S.union (S.fromList $ M.keys as) (S.map beneR cs)
+data Estratto = Estratto {
+	aperti :: M.Map Bene [Evento],
+	chiusi :: S.Set Report,
+	conti_membri :: M.Map User Valore, 
+	conti_responsabili :: M.Map User Valore,
+	membri :: S.Set User, 
+	responsabili ::  M.Map User PublicKey
+	} deriving (Read,Show)
 
-data Conti = Conti {conti_membri, conti_responsabili	:: Map User Valore} deriving (Read,Show)
+type Responso = Either (Evento,String) Evento
 
-type Membri = Set User
-type Responsabili = Set User
+type Validatore = Evento -> Estratto -> Writer [Responso] Estratto
 
-data Extract = Extract {beni :: Beni, conti :: Conti, membri :: Membri, responsabili :: Responsabili} deriving (Read,Show)
+type Componente = Evento -> Estratto -> Either String Estratto
 
-emptyExtract :: Extract
-emptyExtract = Extract (Beni M.empty S.empty) (Conti M.empty M.empty) S.empty S.empty
+validaEvento :: [Componente] -> Validatore
+validaEvento vs e s = case injectM s (map ($e) vs) of 	
+			Left t -> tell [Left (e,t)] >> return s
+			Right s' -> tell [Right e] >> return s'
 
-partitionM :: Monad m => (a -> m Bool) -> [a] -> m ([a],[a])
-partitionM f [] = return ([],[])
-partitionM f (x:xs) = do
-	r <- f x 
-	br <- partitionM f $ xs
-	return . (if r then first (x:) else second (x:)) $ br
+valida :: Validatore -> Conoscenza -> (Estratto,[Responso])
+valida v xs = runWriter $ injectM vuoto (map v xs) where
+	vuoto = Estratto M.empty S.empty M.empty M.empty S.empty M.empty
 
-type Validator = Evento -> State Extract Bool
-type Rigetto = Conoscenza
-validate :: Validator -> Conoscenza -> ((Conoscenza,Rigetto),Extract)
-validate v xs = runState (partitionM v xs) emptyExtract
 
-verifica :: Validator
-verifica = undefined 
--- Fields.hs
+++ Fields.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE StandaloneDeriving #-}
 module Fields where
 
 import Data.List
 import Text.ParserCombinators.ReadP as P
 import Data.Char
 import Control.Applicative
+import Codec.Crypto.RSA
+import Data.ByteString.Lazy.Char8 
+
+
+deriving instance Read PublicKey
+
+newtype Firma = Firma ByteString
+instance Show Firma where
+	show (Firma bs) = "Firma "++ show (Data.List.map ord . unpack $ bs)
+instance Read Firma where
+	readsPrec _ = P.readP_to_S (string "Firma" >> P.skipSpaces >> Firma . pack . Data.List.map chr <$> P.readS_to_P reads) 
 
 
 
