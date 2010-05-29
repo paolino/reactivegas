9d1ca8500e8ba3bf985b413cd8a2d29673b0092e
paolino <paolo.veronelli@gmail.com>
2010-05-29 21:43:55 +0200

    aggiunta la logica del prelievo motivato

--- Eventi/Accredito.hs
+++ Eventi/Accredito.hs
@@ -44,13 +44,13 @@ instance Show Movimento where
 
 
 -- | evento esterno che interessa il controllo del credito o del saldo
-data EsternoAccredito = Accredito Utente DEuro | Saldo Utente DEuro 
+data EsternoAccredito = Accredito Utente DEuro | Addebito Utente String DEuro | Saldo Utente DEuro 
 
 
 instance Show EsternoAccredito where
 	show (Accredito u f) = "accredito a " ++ quote u ++ " di " ++ show f
 	show (Saldo u f) = "movimento dalla cassa di " ++ quote u ++ " di " ++ show f
-
+	show (Addebito u s f) = "addebito a " ++ quote u ++ " di " ++ show f ++ " per " ++ quote s
 instance Read EsternoAccredito where
 	readPrec = let 
 		acc = do
@@ -59,18 +59,26 @@ instance Read EsternoAccredito where
 			string " di "
 			f <- reads'
 			return $ Accredito u f
+		add = do 
+			string "addebito a "
+			u <- phrase
+			string " di "
+			f <- reads'
+			string " per "
+			s <- phrase
+			return $ Addebito u s f
 		sal = do
 			string "movimento dalla cassa di "
 			u <- phrase
 			string " di "
 			f <- reads'
 			return $ Saldo u f
-		in lift $ acc <++ sal
+		in lift $ acc <++ sal <++ add
 -- | priorita' per gli eventi del modulo
 priorityAccredito = R k where
 	k (Accredito _ _) = -35
 	k (Saldo _ _) = -35
-
+	k (Addebito _ _ _) = -34
 -- | stato degli accrediti utente
 data Conti = Conti [(Utente,Euro)] deriving (Read, Show,Eq)
 
@@ -118,22 +126,31 @@ reazioneAccredito :: (
 reazioneAccredito = soloEsterna reattoreAccredito where
 	reattoreAccredito (first validante -> (wrap,Accredito u dv)) = wrap $ \r -> do
 		fallimento (r == u) "aggiornamento del proprio credito di utente"
-		accredita u dv $ "versamento attraverso il cassiere " ++ r
-		salda r dv $ "funzione di cassiere per " ++ u
-		loggamus $ "accreditate " ++ show dv ++ " a " ++ u
+		fallimento (dv $^ 0 <= 0) "accredito negativo"
+		accredita u dv $ "versamento presso il cassiere " ++ quote r
+		salda r dv $ "funzione di cassiere per " ++ quote u
+		loggamus $ "accreditate " ++ show dv ++ " a " ++ quote u
 		return (True,nessunEffetto)	
+	reattoreAccredito (first validante -> (wrap,Addebito u s dv)) = wrap $ \r -> do
+		fallimento (r == u) "aggiornamento del proprio credito di utente"
+		fallimento (dv $^ 0 <= 0) "prelievo negativo o nullo"
+		accredita u (opposite dv) $ "prelievo attraverso il cassiere " ++ quote r ++ " per " ++ quote s
+		salda r (opposite dv) $ "funzione di cassiere per " ++ quote u
+		loggamus $ "prelevate " ++ show dv ++ " a " ++ quote u ++ " per " ++ s
+		return (True,nessunEffetto)
 	reattoreAccredito (first validante -> (wrap ,Saldo u dv)) = wrap $ \r -> do
 		esistenzaResponsabile u
 		fallimento (u == r) "movimento di denaro riferito ad una cassa sola"
 		fallimento (dv $^ 0 <= 0) "saldo negativo o nullo"
-		salda r dv $ "trasferiti dalla cassa di " ++ u
-		salda u (opposite dv) $ "trasferiti alla cassa di " ++ r
-		loggamus $ "spostati " ++ show dv ++ " dalla cassa di " ++ u ++ " alla cassa di " ++ r
+		salda r dv $ "trasferiti dalla cassa di " ++ quote u
+		salda u (opposite dv) $ "trasferiti alla cassa di " ++ quote r
+		loggamus $ "spostati " ++ show dv ++ " dalla cassa di " ++ quote u ++ " alla cassa di " ++ quote r
 		return (True,nessunEffetto)
 
 -- | costruttore di eventi per il modulo di accredito
 costrEventiAccredito :: (Monad m, ParteDi Responsabili s, SUtente `ParteDi` s, ParteDi Anagrafe s) => CostrAction m c EsternoAccredito s
-costrEventiAccredito s kp kn = 	[("aggiornamento credito di un utente",eventoAccredito) 
+costrEventiAccredito s kp kn = 	[("versamento sul conto di un utente",eventoAccredito) 
+				,("prelievo motivato dal conto di un utente",eventoAddebito) 
 				,("ricezione saldo da un responsabile", eventoSaldo)
 				] 
 	where
@@ -146,7 +163,17 @@ costrEventiAccredito s kp kn = 	[("aggiornamento credito di un utente",eventoAcc
 		when (null us') $ throwError "non ci sono altri utenti"
 		u <- scelte us' "utente interessato dall'aggiornamento"
 		n <- libero $ "somma da accreditare sul conto di " ++ u
-		return $ Accredito u n
+		return $ Accredito u n	
+	eventoAddebito = run $ do
+		us <- asks utenti 
+		SUtente un <- asks see
+		when (isNothing un) $ throwError "manca la scelta del responsabile autore"
+		let us' = map (id &&& id) $ filter ((/=) $ fromJust un)  us
+		when (null us') $ throwError "non ci sono altri utenti"
+		u <- scelte us' "utente interessato dall'aggiornamento"
+		n <- libero $ "somma da prelevare dal conto di " ++ u
+		s <- libero $ "motivazione del prelievo"
+		return $ Addebito u s n 
 	eventoSaldo = run $ do
 		(rs,_) <- asks responsabili 
 		SUtente un <- asks see
