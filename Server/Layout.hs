aebc5427dfb256a36b0834e3e35a956a42b4199c
paolino <paolo.veronelli@gmail.com>
2011-01-29 12:51:38 +0100

    jqq89

--- Server/Layout.hs
+++ Server/Layout.hs
@@ -19,11 +19,11 @@ metadata = header << 	[
 		thelink ! [rel "icon", href "/static/favicon.ico"] << noHtml,
 		thetitle << "Amministrazione G.A.S.",
 		meta ! [httpequiv "Content-Type", content "text/html;charset=utf8;"],
-		thelink ! [thetype "text/css", href "static/css/start/jquery-ui-1.8.5.custom.css", rel "Stylesheet"]
+		thelink ! [thetype "text/css", href "static/css/start/jquery-ui-1.8.9.custom.css", rel "Stylesheet"]
 			<<noHtml,
-		script ! [thetype "text/javascript", src "/static/js/jquery-1.4.2.min.js"]
+		script ! [thetype "text/javascript", src "/static/js/jquery-1.4.4.min.js"]
 			<<noHtml,
-		script ! [thetype "text/javascript", src "/static/js/jquery-ui-1.8.5.custom.min.js"] << noHtml,
+		script ! [thetype "text/javascript", src "/static/js/jquery-ui-1.8.9.custom.min.js"] << noHtml,
 		script ! [thetype "text/javascript", src "/static/help.js"] << noHtml
 	] 
 piede = thediv ! [theclass "pedata"] << ulist << 	[
